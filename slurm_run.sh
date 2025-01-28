996273a6d1c7c6af32c6c04f8b541353896cace6
jadedavis5 <jdavis@setonix-02.head.cm.setonix.pawsey.org.au>
2025-01-28 12:21:50 +0800

    make chloroplast optional

--- slurm_run.sh
+++ slurm_run.sh
@@ -16,8 +16,6 @@ module load nextflow/23.10.0
 
 module load singularity/4.1.0-nompi
 
-genome=/scratch/fl3/jdavis/REFERENCES/RGT_2_2024/220816_RGT_Planet_pseudomolecules_and_unplaced_contigs_CPclean.fasta
-gff=/scratch/fl3/jdavis/REFERENCES/RGT_2_2024/RGT_Planet_v2.gff
 
-nextflow run main.nf -profile pawsey_setonix,singularity --nanopore_reads '/scratch/fl3/jdavis/rna_011_ptt_f3.2_RAW-READS/rna_011_ptt_f32_*' \
---genome $genome --ref_annotation $gff -resume
+nextflow run main.nf -profile pawsey_setonix,singularity --nanopore_reads '../../rna_011_ptt_f3.2_RAW-READS/SMALL-*' --chloroplast '/scratch/fl3/jdavis/REFERENCES/NC_056985_chloroplast.fasta' \
+--genome '/scratch/fl3/jdavis/REFERENCES/RGT_2_2024/220816_RGT_Planet_pseudomolecules_and_unplaced_contigs_CPclean_CHR1H.fasta' --ref_annotation '/scratch/fl3/jdavis/REFERENCES/RGT_2_2024/RGT_Planet_v2.gtf' -resume
